96864e530ce8c8993cbc3c026bd8ada7ec989c6b
AzizMadisGroup <abdoul-aziz.traore@madisgroup.net>
2023-12-05 17:04:06 +0000

    firebase admin

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from functions import send_notification
+import firebase_admin
+from firebase_admin import  credentials
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from datetime import datetime
+
+
+cred = credentials.Certificate("./credentials.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -15,21 +22,51 @@ class User(db.Model):
     phone_number = db.Column(db.String(15), unique=True, nullable=False)
     pin_code = db.Column(db.String(8), nullable=False)
 
+
 class Transactions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     transaction_type = db.Column(db.String(50), nullable=False)
     reference = db.Column(db.String(50), nullable=False)
-    method = db.Column(db.String(50), nullable=False)
-    amount = db.Column(db.String(20), nullable=False)
+    original_amount = db.Column(db.Float, nullable=False)  # Nouvelle colonne pour stocker le montant original
+    amount = db.Column(db.Float, nullable=False)  # Montant après déduction des frais
     source_account = db.Column(db.String(20), nullable=False)
     destination_account = db.Column(db.String(20), nullable=True)
-    date = db.Column(db.String(20), nullable=False)
-    hours = db.Column(db.String(20), nullable=False)
+    date = db.Column(db.String(20), nullable=False)  #  # Stocke la date comme un objet date
+    hour = db.Column(db.String(8), nullable=False) 
     status = db.Column(db.String(20), nullable=False)
     description = db.Column(db.String(255), nullable=True)
     payment_method = db.Column(db.String(50), nullable=True)
-    fee = db.Column(db.String(20), nullable=True)
+    fee = db.Column(db.Float, nullable=True)
+    transaction_token = db.Column(db.String(255), nullable=True)
+    
+    
+@app.route('/status-transaction', methods=['POST'])
+def updateTransaction():
+    
+    data = request.get_json()
 
+    # Extraire les informations du JSON
+    reference = data['reference']
+    new_status = data['status']
+
+
+    try:
+        # Récupérer la transaction à partir de la base de données
+        transaction = Transactions.query.filter_by(reference=reference).first()
+
+        if transaction:
+            # Mettre à jour le statut de la transaction
+            transaction.status = new_status
+            db.session.commit()
+
+            # Envoi de la notification avec Firebase Admin
+            send_notification(transaction)
+
+            return make_response(jsonify({'message': 'Transaction status updated successfully'}), 200)
+        else:
+            return make_response(jsonify({'message': 'Transaction not found'}), 404)
+    except Exception as e:
+        return make_response(jsonify({'message': str(e)}), 500)
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -84,29 +121,35 @@ def verify_phone_number():
         return make_response(jsonify({'message': 'Phone number is available'}), 200)
 
 #TRANSACTIONS
-
-
 @app.route('/make-deposit', methods=['POST'])
 def make_deposit():
     data = request.get_json()
 
-    # Obtenir automatiquement la date et l'heure actuelles
     current_datetime = datetime.now()
-    current_date = current_datetime.strftime('%Y-%m-%d')
-    current_hours = current_datetime.strftime('%H:%M:%S')
+    current_date = current_datetime.date()  # Utilise la date actuelle comme un objet date
+
+    # Génération automatique de la référence pour le dépôt
+    reference = f'MADIS00XX{data["sourceAccount"][-2:]}{current_date.strftime("%Y%m%d")}'
+
+    # Calcul des frais pour le dépôt (1%)
+    fee_percentage = 0.01
+    original_amount = data['amount']
+    fee = original_amount * fee_percentage
+    final_amount = original_amount - fee
 
     new_transaction = Transactions(
         transaction_type='deposit',
-        reference=data['reference'],
-        method=data['method'],
-        amount=data['amount'],
+        reference=reference,
+        original_amount=original_amount,
+        amount=final_amount,
         source_account=data['sourceAccount'],
-        date=current_date,
-        hours=current_hours,
-        status='en cours',  # Définir le statut par défaut
+        date=current_datetime.strftime('%d-%m-%Y'),
+        hour=current_datetime.strftime('%H:%M:%S'),
+        status='en cours',
         description=data.get('description'),
         payment_method=data.get('paymentMethod'),
-        fee=data.get('fee')
+        fee=fee,
+        transaction_token=data.get('transactionToken')
     )
 
     db.session.add(new_transaction)
@@ -118,24 +161,32 @@ def make_deposit():
 def make_transfer():
     data = request.get_json()
 
-    # Obtenir automatiquement la date et l'heure actuelles
     current_datetime = datetime.now()
-    current_date = current_datetime.strftime('%Y-%m-%d')
-    current_hours = current_datetime.strftime('%H:%M:%S')
+    current_date = current_datetime.date()  # Utilise la date actuelle comme un objet date
+
+    # Génération automatique de la référence pour le transfert
+    reference = f'MADIS01XX{data["sourceAccount"][-2:]}{data["destinationAccount"][-2:]}{current_date.strftime("%Y%m%d")}'
+
+    # Calcul des frais pour le transfert (1.5%)
+    fee_percentage = 0.015
+    original_amount = data['amount']
+    fee = original_amount * fee_percentage
+    final_amount = original_amount - fee
 
     new_transaction = Transactions(
         transaction_type='transfer',
-        reference=data['reference'],
-        method=data['method'],
-        amount=data['amount'],
+        reference=reference,
+        original_amount=original_amount,
+        amount=final_amount,
         source_account=data['sourceAccount'],
         destination_account=data['destinationAccount'],
-        date=current_date,
-        hours=current_hours,
-        status='en cours',  # Définir le statut par défaut
+        hour=current_datetime.strftime('%H:%M:%S'),
+        date=current_datetime.strftime('%d-%m-%Y'),
+        status='en cours',
         description=data.get('description'),
         payment_method=data.get('paymentMethod'),
-        fee=data.get('fee')
+        fee=fee,
+        transaction_token=data.get('transactionToken')
     )
 
     db.session.add(new_transaction)
@@ -144,7 +195,31 @@ def make_transfer():
     return make_response(jsonify({'message': 'Transfer successful'}), 201)
 
 
+@app.route('/get-transactions', methods=['GET'])
+def get_all_transactions():
+    try:
+        transactions = Transactions.query.all()
+        print(transactions)
+        # Convertir les objets Transactions en un format JSON
+        transactions_json = [{
+            'id': transaction.id,
+            'transactionType': transaction.transaction_type,
+            'reference': transaction.reference,
+            'originalAmount': transaction.original_amount,
+            'amount': transaction.amount,
+            'sourceAccount': transaction.source_account,
+            'destinationAccount': transaction.destination_account,
+            'date': transaction.date,
+            'hours': transaction.hour,
+            'status': transaction.status,
+            'description': transaction.description,
+            'paymentMethod': transaction.payment_method,
+            'fee': transaction.fee
+        } for transaction in transactions]
 
+        return jsonify(transactions_json)
+    except Exception as e:
+        return make_response(jsonify({'message': str(e)}), 500)
 
 if __name__ == '__main__':
     with app.app_context():
---
+++ functions.py
@@ -0,0 +1,35 @@
+from firebase_admin import messaging
+
+# Fonction pour envoyer une notification avec Firebase Admin
+def send_notification(transaction):
+    try:
+        message = messaging.Message(
+            notification=messaging.Notification(
+                
+                
+                title='Madis',
+                body=f'Le statut de votre transaction {transaction.reference} est maintenant {transaction.status}.'
+            ),
+        android=messaging.AndroidConfig(
+  
+        priority='normal',
+        notification=messaging.AndroidNotification(
+            visibility=messaging.Notification.v,
+            icon='stock_ticker_update',
+            color='#f45342',
+      image='https://www.pngitem.com/pimgs/m/359-3590778_image-of-pizza-monster-sticker-pizza-monster-png.png',
+      
+        ),
+    ),
+            data={
+                "title":"Etat de la transaction",
+                "message":f'Le statut de votre transaction {transaction.reference} est maintenant {transaction.status}.'
+            },
+            token=transaction.transaction_token,  # Remplacez par le vrai token du dispositif
+        )
+
+        # Envoyer le message
+        response = messaging.send(message)
+        print('Notification sent successfully:', response)
+    except Exception as e:
+        print('Error sending notification:', str(e))
